486f39062b599dcb3d8793b8b9950c73526eb7d1
Sarah Khadraoui <skhadrao16@gmail.com>
2024-06-04 16:06:41 -0400

    fairisle code working!

--- fairisle-edit.js
+++ fairisle-edit.js
@@ -2,10 +2,9 @@ var knitout = require('knitout');
 k = new knitout.Writer({carriers:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]});
 k.addHeader('Machine','SWGXYZ');
 k.addHeader('Gauge','15');
-k.addHeader('X-Presser-Mode','auto');
 k.addHeader('Position','Center');
 
-testPattern = require('./pattern.js');
+const {testPattern} = require('./pattern.js');
 
 let startingRow = [0,0,0,0,0,0,0,0,0,0,0,0,0];
 
@@ -13,65 +12,116 @@ let carrierA = "3";
 let carrierB = "6";
 let height = 65;
 let width = 65;
-let front = width%2;
+let Wmax = width-1; // max width
+let Wmin = 0; // min width
 
-k.inhook(carrier);
+//cast on front bed
 
-for (let s=width; s>0; s--) {
-	if (s%2==front) {
-		k.knit("-", "f"+s, carrier);
-	}
-	else {
-		k.knit("-", "b"+s, carrier);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+k.inhook(carrierA);
+
+for (let s = Wmax; s >= Wmin; s -= 1) {
+	if ((Wmax - s) % 2 == 0) {
+		k.tuck("-", 'f' + s, carrierA);
 	}
 }
-for (let s=1; s<=width; s++) {
-	if (s%2==front) {
-		k.knit("+", "f"+s, carrier);
-	}
-	else{
-		k.knit("+", "b"+s, carrier);
+
+for (let s = Wmin; s <= Wmax; s += 1) {
+	if ((Wmax - s) % 2 !== 0) {
+		k.tuck("+", 'f' + s, carrierA);
 	}
 }
+
+//knit 2 plain rows
+for (let s = Wmax; s >= Wmin; s -= 1) {
+	k.knit("-", 'f' + s, carrierA);
+}
+for (let s = Wmin; s <= Wmax; s += 1) {
+	k.knit("+", 'f' + s, carrierA);
+}
+
+k.releasehook(carrierA);
+k.inhook(carrierB);
+
+for (let s = Wmax; s >= Wmin; s -= 1) {
+	k.knit("-", 'f' + s, carrierB);
+}
+for (let s = Wmin; s <= Wmax; s += 1) {
+	k.knit("+", 'f' + s, carrierB);
+}
+k.releasehook(carrierB);
 
 // Go through each row
-function fairisle (pattern, height, width) {
-	for (let i=0; i<pattern.length; i++) {
-		console.log("starting row: "+i);
-		let thisRow = pattern[i];
+//how to have it repeat based on my height and width??
+function fairisle (pattern) {
+	for (let i=0; i < height; i++) {
+		let thisRow = pattern[i % pattern.length];
+		// console.log(pattern);
+		// console.log(i);
 		let output = "";
 		// In each row, go through each stitch
 		if (i%2 == 0) {
-			for (let j=thisRow.length-1; j>=0; j--){
+			for (let j= width-1; j>=0; j--){
 				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
 				// (all front bed knits)
-				let carrier = carrierB;
-				if (thisRow[j] == 1) {
-					carrier = carrierA;
+				
+				if (thisRow[j % thisRow.length] == 1) {
+					k.knit("-", "f"+j, carrierA);
 				}
-				k.knit("-", "f"+j, carrier);
+				else{
+					k.miss("-", "f"+j, carrierA);
+				}
 			}
+			for (let j= width-1; j>=0; j--){
+				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
+				// (all front bed knits)
+				
+				if (thisRow[j % thisRow.length] == 0) {
+					k.knit("-", "f"+j, carrierB);
+				}
+				else{
+					k.miss("-", "f"+j, carrierB);
+				}
+			}
+
 		}
 
 		else {
-			for (let j=0; j<thisRow.length; j++){
+			for (let j=0; j< width; j++){
 				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
 				// (all front bed knits)
-				let carrier = carrierB;
-				if (thisRow[j] == 1) {
-					carrier = carrierA;
+				//let carrier = carrierB;
+				if (thisRow[j % thisRow.length] == 1) {
+					k.knit("+", "f"+j, carrierA);
 				}
-				k.knit("+", "f"+j, carrier);
+				else{
+					k.miss("+", "f"+j, carrierA);
+				}
+			}
+			for (let j=0; j< width; j++){
+				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
+				// (all front bed knits)
+				//let carrier = carrierB;
+				if (thisRow[j % thisRow.length] == 0) {
+					k.knit("+", "f"+j, carrierB);
+				}
+				else{
+					k.miss("+", "f"+j, carrierB);
+				}
+				
 			}
 		}
 	}
 }
 
 
+fairisle(testPattern);
+
+k.outhook(carrierA);
+
+k.outhook(carrierB);
 
 
-kp(testPattern);
-
+k.write('fairisle-edit.k');
 
 
 
--- kp-edit.js
+++ kp-edit.js
@@ -11,56 +11,31 @@ let startingRow = [0,0,0,0,0,0,0,0];
 
 let carrierA = "3";
 let carrierB = "6";
+let width = 65;
+let height = 65;
+let front = width%2;
 
-// function knit(dir, needle, carrier) {
-// 	console.log("knit", dir, needle, carrier);
-// }
-// function xfer(fromNeedle, toNeedle) {
-// 	console.log("transfer", fromNeedle, toNeedle);
-// }
+k.inhook(carrier);
 
-// Go through each row
-function fairisle (pattern) {
-	for (let i=0; i<pattern.length; i++) {
-		console.log("starting row: "+i);
-		let thisRow = pattern[i];
-		let output = "";
-		// In each row, go through each stitch
-
-		if (i%2 == 0) {
-			for (let j=thisRow.length-1; j>=0; j--){
-				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
-				// (all front bed knits)
-				let carrier = carrierB;
-				if (thisRow[j] == 1) {
-					carrier = carrierA;
-				}
-				k.knit("-", "f"+j, carrier);
-			}
-		}
-
-		else {
-			for (let j=0; j<thisRow.length; j++){
-				// if the pattern says "1" at this location in the pattern, knit with carrierA; otherwise, carrierB
-				// (all front bed knits)
-				let carrier = carrierB;
-				if (thisRow[j] == 1) {
-					carrier = carrierA;
-				}
-				k.knit("+", "f"+j, carrier);
-			}
-		}
+for (let s=width; s>0; s--) {
+	if (s%2==front) {
+		k.knit("-", "f"+s, carrier);
+	}
+	else {
+		k.knit("-", "b"+s, carrier);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+	}
+}
+for (let s=1; s<=width; s++) {
+	if (s%2==front) {
+		k.knit("+", "f"+s, carrier);
+	}
+	else{
+		k.knit("+", "b"+s, carrier);
 	}
 }
 
-// // no transfers needed
-// if ((this == 1 && previous == 1) || (this == 0 && previous == 0))
+k.releasehook(carrier);
 
-// is (this == prev)
-
-// // transfers needed
-// if (this == 0 && previous == 1) {xfer front to back}
-// if (this == 1 && previous == 0) {xfer back to front}
 
 function kp(pattern) {
 	// Go through each row
